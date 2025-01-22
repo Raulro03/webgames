8f13992bcf7dce3b28ab7569ab76f70547cf7e1d
Raulro03 <raulromeraortiz@gmail.com>
2025-01-22 14:34:20 +0100

    Welcome Test y Vista Welcome

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\PageWelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageWelcomeController::class, 'index'])->name('welcome');
 
 Route::middleware([
     'auth:sanctum',
