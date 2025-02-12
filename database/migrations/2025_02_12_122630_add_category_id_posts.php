7633ad4dcae4c21e2a5b85ada4130e968beebbc8
Raulro03 <raulromeraortiz@gmail.com>
2025-02-12 13:32:59 +0100

    detallito en la base de datos

--- database/migrations/2025_02_12_122630_add_category_id_posts.php
+++ database/migrations/2025_02_12_122630_add_category_id_posts.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->foreignId('category_id')->constrained('forum_categories')->onDelete('cascade');
+            $table->foreignId('category_id')->after('user_id')->constrained('forum_categories')->onDelete('cascade');
         });
     }
 
