a70e3996617e32189291e903e0dea464f35f2d78
Raulro03 <raulromeraortiz@gmail.com>
2025-05-09 13:11:35 +0200

    Creacion Livewire cruds

---
+++ app/Livewire/CharactersManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CharactersManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.characters-manager');
+    }
+}
