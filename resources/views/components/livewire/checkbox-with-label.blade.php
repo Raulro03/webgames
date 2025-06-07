cc3e36bc52ba79431eebee0710f210537058c408
Raulro03 <raulromeraortiz@gmail.com>
2025-06-07 20:57:29 +0200

    Mejora componente label

--- resources/views/components/livewire/checkbox-with-label.blade.php
+++ resources/views/components/livewire/checkbox-with-label.blade.php
@@ -3,5 +3,5 @@
            wire:click="{{ $toggleMethod }}({{ $item->id }})"
            @if(isset($selectedItems[$item->id])) checked @endif
            class="rounded border-gray-300 text-purple-600 shadow-sm focus:ring focus:ring-purple-200">
-<label class="ml-2 text-sm text-gray-700">{{ $item->{$labelField} }}</label>
+<x-label class="ml-2 text-sm text-gray-700">{{ $item->{$labelField} }}</x-label>
 
